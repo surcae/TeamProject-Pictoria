83d0bba73b982b9313b304e082032d2fe7c0af82
DESKTOP-CAAMBBE\surcae_laptop <surcae@gmail.com>
2018-04-10 18:07:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/surcae_laptop/pictoria/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.surcae_laptop.pictoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
