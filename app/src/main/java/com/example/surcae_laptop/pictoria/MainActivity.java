3b6b3af294ffd6b0215accecea9fa3ebb0678804
DESKTOP-CAAMBBE\surcae_laptop <surcae@gmail.com>
2018-04-10 18:19:24 +0900

    Test Commit

--- app/src/main/java/com/example/surcae_laptop/pictoria/MainActivity.java
+++ app/src/main/java/com/example/surcae_laptop/pictoria/MainActivity.java
@@ -10,4 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Main activity 수정 테스트 Commit 용
+
 }
