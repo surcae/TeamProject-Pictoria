7068b8f53c14c032447afa668a36227dcdb0fbfe
surcae <surcae@gmail.com>
2018-05-05 23:20:39 +0900

    Initial Activity has changed -> Loading Activity Add Splash Components (xml, img, code etc...)

--- app/src/main/java/com/example/surcae_laptop/pictoria/LoadingActivity.java
+++ app/src/main/java/com/example/surcae_laptop/pictoria/LoadingActivity.java
@@ -1,4 +1,23 @@
 package com.example.surcae_laptop.pictoria;
 
-public class LoadingActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+                Intent mainIntent = new Intent(LoadingActivity.this,LoginActivity.class);
+                LoadingActivity.this.startActivity(mainIntent);
+                LoadingActivity.this.finish();
+            }
+        }, 1000);
+    }
 }
