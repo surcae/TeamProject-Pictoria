a7837526905fbbea25d9c49f34eb537c487c72d6
hbiny <hbiny92@gmail.com>
2018-06-11 21:19:20 +0900

    * 네비게이션 메뉴수정 - favorite + Profile * Profile, setting fragment, layout 추가 * 샘플 이미지 추가

--- app/src/main/java/com/example/surcae_laptop/pictoria/SettingFragment.java
+++ app/src/main/java/com/example/surcae_laptop/pictoria/SettingFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /* Setting 구현 예정 */
 
@@ -17,7 +20,24 @@ public class SettingFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //ui 만들어서 view 리턴
-        return inflater.inflate(R.layout.fragment_setting,container,false);
+        final String[] values = new String[]{
+                "로그인","로그아웃","지원","고객센터","문제 신고","소개","광고","개인정보처리방침","서비스 약관",
+        };
+
+        View view = inflater.inflate(R.layout.fragment_setting,container,false) ;
+        ListView listview = (ListView) view.findViewById(R.id.setting_list) ;
+
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(
+                getActivity(),android.R.layout.simple_list_item_1,values);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        return view;
     }
 }
