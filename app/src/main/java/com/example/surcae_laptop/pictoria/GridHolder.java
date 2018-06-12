1e8b29b97cb2a467c3b5126c1d54b595710da34c
hbiny <hbiny92@gmail.com>
2018-06-12 15:55:42 +0900

    * image pop up

--- app/src/main/java/com/example/surcae_laptop/pictoria/GridHolder.java
+++ app/src/main/java/com/example/surcae_laptop/pictoria/GridHolder.java
@@ -1,21 +1,56 @@
 package com.example.surcae_laptop.pictoria;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GridHolder extends RecyclerView.ViewHolder {
+import static android.support.v4.content.ContextCompat.startActivity;
+
+public class GridHolder extends RecyclerView.ViewHolder{
 
     ImageView imageView;
+    ImageView imageView_pop;
+
     TextView textView;
 
-    public GridHolder(View itemView) {
+    Dialog dialog;
+
+    private final Context context;
+
+    public GridHolder(final View itemView) {
         super(itemView);
+        context=itemView.getContext();
 
         imageView=itemView.findViewById(R.id.s_image);
         textView=itemView.findViewById(R.id.t_caption);
+        imageView.setClickable(true);
 
+        //recyclerview의 imageview 클릭시 팝업 형태 구현
+        imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    customdialog(v);
+                }
+        });
+    }
+
+        public void customdialog(View v) {
+
+            imageView_pop=(ImageView) v.findViewById(R.id.image_pop);
+
+            dialog= new Dialog(MainActivity.getContext());
+            dialog.setContentView(R.layout.popup);
+            dialog.show();
 
     }
+
+
 }
